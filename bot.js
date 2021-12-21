68d04f2daa02364cec9a8bc811cad8521f338997
mohammad ameer <mohammadameerabdallah@gmail.com>
2021-12-21 23:11:16 +0200

    return env

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ import { Telegraf } from "telegraf";
 import database from "./database.js";
 import messages from "./messages.js";
 
-const bot = new Telegraf("5050130701:AAEKOp5s-1J-f0uvGRwCNHA3usJ82dR0dyc");
+const bot = new Telegraf(process.env.BOT_TOKEN);
 
 const MAX_QUESTIONS = 50;
 
