f965ee5c2a00d4ef273aebbe2d7c331d25fe928d
mohammad ameer <mohammadameerabdallah@gmail.com>
2021-12-23 10:46:24 +0200

    update welocome message

--- messages.js
+++ messages.js
@@ -1,9 +1,9 @@
 export default {
-  startMessage: `Hey There 👋, welcom to Marhaba Defi Questions this bot was created to answer only shariah related quetions Bot \n\nusing this bot you can: \n\nadd a new question by using /q {your question after command} or /question {your question after command} command then the question. \n\nget all questions by using /all or /allquestions command. \n\nupvote a question by using /upvote {question number after command} or /up {question number after command} command then the question number. \n\nquestions ar sorted by upvotes.`,
+  startMessage: `Hey There 👋, welcom to Marhaba Defi Questions this bot was created to answer shariah related questions on crypto only  \n\nusing this bot you can: \n\nsearch questions by @mrhbqabo {your question} \n\nadd a new question by using /q {your question after command} or /question {your question after command} command then the question. \n\nget all questions by using /all or /allquestions command. \n\nupvote a question by using /upvote {question number after command} or /up {question number after command} command then the question number. \n\nquestions ar sorted by upvotes.`,
   questionsMax: "The maximum number of questions has been reached.",
   notCommandMessage:
-    "to use this bot you can use this commands: \n\n/q {your question after command} or /question {your question after command} to ask a question. \n\n/all or /allquestions to see all the questions. \n\n/upvote {question number after command} or /up {question number after command} to upvote a question. ",
+    "to use this bot you can use this commands: \n\n/q {your question after command} to ask a question. \n\n/all to see all the questions. \n\n/upvote {question number after command} to upvote a question. ",
   questionMinMessage: "The question must be at least 5 words long.",
   noQuestionsMessage:
-    "There are no questions you can add new one by using /q or /question command. then your question",
+    "There are no questions you can add new one by using /q {your question after command}. then your question",
 };
