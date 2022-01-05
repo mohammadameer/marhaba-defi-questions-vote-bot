2db58f62d2faa571c924b775f745dfcf2b41d855
mohammad ameer <mohammadameerabdallah@gmail.com>
2022-01-05 19:36:54 +0200

    update messages

--- messages.js
+++ messages.js
@@ -1,8 +1,8 @@
 export default {
-  startMessage: `Hey There 👋, welcom to Marhaba Defi Questions this bot was created to answer shariah related questions on crypto only  \n\nusing this bot you can: \n\nsearch questions by @mrhbqabo {your question} \n\nadd a new question by using /q {your question after command in the same message}. \n\nget all questions by using /all . \n\nupvote a question by using / /up {question number after command in the same message} . \n\nquestions ar sorted by upvotes.`,
+  startMessage: `Hey There 👋, welcom to Marhaba Defi Questions this bot was created to answer shariah related questions on crypto only  \n\nusing this bot you can: \n\nsearch questions by @mrhbqabo {your question} \n\nadd a new question by using /q {your question after command in the same message}. \n\nsearch questions by using mentioning the bot and then quetion in the same message \n \`@mrhbqabot {your question}\` . \n\nupvote a question by using / /up {question number after command in the same message} . \n\nquestions ar sorted by upvotes.`,
   questionsMax: "The maximum number of questions has been reached.",
   notCommandMessage:
-    "to use this bot you can use this commands: \n\n/q {your question after command} to ask a question. \n\n/all to see all the questions. \n\n/upvote {question number after command} to upvote a question. ",
+    "to use this bot you can use this commands: \n\n/q {your question after command} to ask a question. \n\n/search questions by using mentioning the bot and then quetion in the same message \n `@mrhbqabot {your question}` . \n\n/upvote {question number after command} to upvote a question. ",
   questionMinMessage: "The question must be at least 5 words long.",
   noQuestionsMessage:
     "There are no questions you can add new one by using /q {your question after command}. then your question",
